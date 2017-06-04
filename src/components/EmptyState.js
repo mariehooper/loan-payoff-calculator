6028398d8f906f7b5d2cdf1bd7e3ff6e4da53a52
Marie Hooper <marie.ashtari@gmail.com>
2017-06-04 10:35:48 -0400

    refactor app into components, add empty state
    closes #10

--- src/components/EmptyState.js
+++ src/components/EmptyState.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
-// import './EmptyState.css';
+import './EmptyState.css';
 
 export default function EmptyState({ openModal }) {
   return (
