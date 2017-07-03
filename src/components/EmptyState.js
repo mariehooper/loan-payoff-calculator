348b62b454f20d9ecc2ef532a1370c19a4c96156
Marie Hooper <marie.ashtari@gmail.com>
2017-07-03 09:11:13 -0400

    update empty state graphic

--- src/components/EmptyState.js
+++ src/components/EmptyState.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import pig from '../piggy-bank.png';
+import pig from '../piggy-bank-2.png';
 import Button from './Button';
 import './EmptyState.css';
 
